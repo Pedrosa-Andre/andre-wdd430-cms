f5d599cd3e3847fec1576b373a30a4d15721bf12
André Pedrosa <ped17006@byui.edu>
2024-10-12 17:29:02 -0300

    Week 04 Assignment

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'app-document-list',
   templateUrl: './document-list.component.html',
-  styleUrl: './document-list.component.css'
+  styleUrl: './document-list.component.css',
 })
 export class DocumentListComponent {
+  @Output() selectedDocumentEvent = new EventEmitter();
 
+  documents: Document[] = [
+    new Document('1', 'Name 1', 'Description 1', 'url 1', null),
+    new Document('2', 'Name 2', 'Description 2', 'url 2', null),
+    new Document('3', 'Name 3', 'Description 3', 'url 3', null),
+    new Document('4', 'Name 4', 'Description 4', 'url 4', null),
+    new Document('5', 'Name 5', 'Description 5', 'url 5', null),
+  ];
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
+  }
 }
